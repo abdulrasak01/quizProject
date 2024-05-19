e0cab7593b2eefb2249df03e1f795abcadee94bf
abdulrasak <abdulrasakn2004@gmail.com>
2024-05-19 12:38:37 +0530

    Finished

--- src/ResultPage.js
+++ src/ResultPage.js
@@ -1,17 +1,22 @@
-import React from 'react'
-import { useContext } from 'react'
-import { AppContext } from './App'
+import React from "react";
+import { useContext } from "react";
+import { AppContext } from "./App";
 
 const ResultPage = () => {
-  const {questions,score} = useContext(AppContext)
+  const { questions, score } = useContext(AppContext);
   return (
     <div>
-        <h1 className='text-3xl font-bold my-5'>Result:</h1>
-        {questions.length == score ?
-        <div className='ml-5 text-2xl font-bold '>Congratulations, You Got Full Score</div>:null}
-        <div className='ml-5 text-2xl font-bold'>Your Score is {score} out of {questions.length}</div>
+      <h1 className="text-3xl font-bold my-5">Result:</h1>
+      {questions.length == score ? (
+        <div className="ml-5 text-2xl font-bold ">
+          Congratulations, You Got Full Score
+        </div>
+      ) : null}
+      <div className="ml-5 text-2xl font-bold">
+        Your Score is {score} out of {questions.length}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ResultPage
+export default ResultPage;
